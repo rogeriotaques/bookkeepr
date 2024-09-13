dfdaacb169f16219475ece9299580dba209f5fc4
Rogerio Taques <1456123+rogeriotaques@users.noreply.github.com>
2024-09-13 10:07:06 +0900

    wip: authonboarding

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -8,5 +8,20 @@ exports.authenticateUser = async (req, res) => {
   res.json({ isAuthenticated: !!(authData && authData.value === hashedPassword) });
 };
 
-// TODO: Implement a set password method
-// TODO: Implement a method to check if password is set
+exports.saveUserPassword = async (req, res) => {
+  const { password } = req.body;
+  const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
+
+  await global
+    .knex('config')
+    .insert([
+      { key: 'passwd', value: hashedPassword },
+      { key: 'usePasswd', value: 1 },
+    ])
+    .onConflict('key')
+    .merge();
+
+  res.json({ success: true });
+};
+
+// TODO: Implement a method to check if user is authenticated
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,5 +8,6 @@ const authController = require('@/controllers/auth.js');
 const router = require('express').Router();
 
 router.post('/', authController.authenticateUser);
+router.post('/passwd', authController.saveUserPassword);
 
 module.exports = router;
