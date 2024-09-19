4055a0726badad20fb33487517dc6e48c3243c6e
Rogerio Taques <1456123+rogeriotaques@users.noreply.github.com>
2024-09-19 22:02:08 +0900

    chore: minor tweak for bump.sh

--- bump.sh
+++ bump.sh
@@ -15,6 +15,9 @@ sed -i '' "s/\"version\": \"$CURRENT_VERSION\"/\"version\": \"$NEW_VERSION\"/g" 
 # Update the version in backend/package.json
 sed -i '' "s/\"version\": \"$CURRENT_VERSION\"/\"version\": \"$NEW_VERSION\"/g" backend/package.json
 
+echo "Version bumped to $NEW_VERSION in the package.json files"
+echo ""
+
 # Push changes to git
 git add frontend/package.json backend/package.json
 git commit -m "Bump version to $NEW_VERSION"
@@ -24,17 +27,21 @@ git push --tags
 
 
 echo "Version updated to $NEW_VERSION"
+echo ""
 
 # Build the new Docker image
 docker build --platform linux/amd64 -t abtzco/bookkeepr .
 echo "Docker image built"
+echo ""
 
 # Tag the new Docker image
 docker image tag abtzco/bookkeepr abtzco/bookkeepr:$NEW_VERSION
 echo "Docker image tagged"
+echo ""
 
 # Push the new Docker image
 docker push abtzco/bookkeepr
 echo "Docker image pushed"
+echo ""
 
 echo "All done!"
